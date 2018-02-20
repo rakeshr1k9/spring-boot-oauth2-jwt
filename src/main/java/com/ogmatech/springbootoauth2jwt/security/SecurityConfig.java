c1961e2c4ed56c5c97f923aa92df79e0570ac1ce
rakeshr1k9 <rakeshr1k9@gmail.com>
2018-02-20 22:53:50 +0530

    security configuration

---
+++ src/main/java/com/ogmatech/springbootoauth2jwt/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.ogmatech.springbootoauth2jwt.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .formLogin().disable() //disable form authentication
+                .anonymous().disable() //disable anonymous user
+                .authorizeRequests().anyRequest().denyAll(); //denying all access
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth
+                .inMemoryAuthentication() //creating user in memory
+                .withUser("user")
+                .password("password1").roles("USER")
+                .and().withUser("admin")
+                .password("password2").authorities("ROLE_ADMIN");
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        //provides the default AuthenticationManager as a Bean
+        return super.authenticationManagerBean();
+    }
+}
