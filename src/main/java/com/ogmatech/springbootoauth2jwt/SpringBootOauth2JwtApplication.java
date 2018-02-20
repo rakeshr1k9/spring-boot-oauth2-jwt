67e0cc21447b4ec178a611a4586e0f8477dd3452
rakeshr1k9 <rakeshr1k9@gmail.com>
2018-02-20 20:44:41 +0530

    Initial Git

---
+++ src/main/java/com/ogmatech/springbootoauth2jwt/SpringBootOauth2JwtApplication.java
@@ -0,0 +1,12 @@
+package com.ogmatech.springbootoauth2jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOauth2JwtApplication.class, args);
+	}
+}
