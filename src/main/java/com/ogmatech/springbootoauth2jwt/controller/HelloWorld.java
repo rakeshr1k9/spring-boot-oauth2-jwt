ac7edd7b00b15d9e2829a7d98dac0c84c8496435
rakeshr1k9 <rakeshr1k9@gmail.com>
2018-02-23 01:08:27 +0530

    working controller with oauth and jwt

--- src/main/java/com/ogmatech/springbootoauth2jwt/controller/HelloWorld.java
+++ src/main/java/com/ogmatech/springbootoauth2jwt/controller/HelloWorld.java
@@ -1,12 +1,37 @@
 package com.ogmatech.springbootoauth2jwt.controller;
 
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class HelloWorld {
-    @RequestMapping("/")
+    @GetMapping("/")
     public String home(){
         return "Hello Raki";
+    }
+
+    @GetMapping("/api/test")
+    public String apitest(){
+        return "Hello api Test";
+    }
+
+    @GetMapping("/api/hello")
+    public String helloUser(){
+        //The authenticated user can be fetched using the SecurityContextHolder
+        String username = new SecurityContextHolder().getContext().getAuthentication().getName();
+        return String.format("Hello '%s'!", username);
+    }
+
+    @GetMapping("/api/admin")
+    // If controller request asks for the Principal user in
+    // the method declararion Spring security will provide it.
+    public String helloAdmin(Principal principal){
+        return String.format("Welcom '%s'!", principal.getName());
+
     }
 }
--- src/main/java/com/ogmatech/springbootoauth2jwt/security/SecurityConfig.java
+++ src/main/java/com/ogmatech/springbootoauth2jwt/security/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.ogmatech.springbootoauth2jwt.security;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,12 +12,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity(debug = true)
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http
                 .formLogin().disable() //disable form authentication
                 .anonymous().disable() //disable anonymous user
+                .httpBasic().and()
+                //restricting access to authenticated users
                 .authorizeRequests().anyRequest().denyAll(); //denying all access
     }
 
